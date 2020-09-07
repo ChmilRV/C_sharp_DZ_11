ecdbc83c11c2c81c2d99342295c75d03ab0539f6
Chmil Ruslan <chmilrv@users.noreply.github.com>
2020-09-07 15:11:43 +0300

    1511

--- C_sharp_DZ_11_1/Program.cs
+++ C_sharp_DZ_11_1/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using static System.Console;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 /*С помощью класса XmlTextWriter напишите приложение, сохраняющее в XML-файл информацию о заказах.
 Каждый заказ представляет собой несколько товаров. Информацию, характеризующую заказы и товары
 необходимо разработать самостоятельно.
@@ -26,10 +28,10 @@ namespace C_sharp_DZ_11_1
         }
         public virtual void ProductReport()
         {
-            Console.WriteLine($"Артикул: {_article:D4}");
-            Console.WriteLine($"Наименование: {_name}");
-            Console.WriteLine($"Цена: {_price:C2}");
-            Console.WriteLine($"Остаток: {_balance}\n");
+            WriteLine($"Артикул: {_article:D4}");
+            WriteLine($"Наименование: {_name}");
+            WriteLine($"Цена: {_price:C2}");
+            WriteLine($"Остаток: {_balance}\n");
         }
     }
     class HouseholdChemicals : Product      //Бытовая химия
@@ -42,7 +44,7 @@ namespace C_sharp_DZ_11_1
         }
         public override void ProductReport()
         {
-            Console.WriteLine($"Категория: {_categoryHoushold}");
+            WriteLine($"Категория: {_categoryHoushold}");
             base.ProductReport();
         }
     }
@@ -56,7 +58,7 @@ namespace C_sharp_DZ_11_1
         }
         public override void ProductReport()
         {
-            Console.WriteLine($"Категория: {_categoryFood}");
+            WriteLine($"Категория: {_categoryFood}");
             base.ProductReport();
         }
     }
@@ -68,6 +70,50 @@ namespace C_sharp_DZ_11_1
 
         static void Main(string[] args)
         {
+
+            Product[] HousholdTest = {
+                new HouseholdChemicals(001,"Порошок", 38.66, 1457),
+                new HouseholdChemicals(002,"Зубная паста", 73.26, 2357),
+                new HouseholdChemicals(003,"Мыло", 18.28, 4330)
+            };
+            Product[] FoodTest = {
+                new Food(001,"Хлеб", 18.34, 5347),
+                new Food(002,"Масло", 52.61, 3712),
+                new Food(003,"Молоко", 25.28, 6743),
+                new Food (004, "Яблоко", 35.92, 9847)
+            };
+
+
+
+            XmlTextWriter writer = null;
+
+            try
+            {
+                writer = new XmlTextWriter("Cars.xml", System.Text.Encoding.Unicode);
+                writer.Formatting = Formatting.Indented;
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Cars");
+                writer.WriteStartElement("Car");
+                writer.WriteAttributeString("Image", "MyCar.jpeg");
+                writer.WriteElementString("Manufactured", "La Hispano Suiza de Automovils");
+                writer.WriteElementString("Model", "Alfonso");
+                writer.WriteElementString("Year", "1912");
+                writer.WriteElementString("Color", "Black");
+                writer.WriteElementString("Speed", "130");
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+
+                WriteLine("The Cars.xml file is generated!");
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
 
 
 
