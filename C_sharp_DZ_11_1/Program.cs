0214059291f2321bb6e2efaab3be67ced137ac7b
Chmil Ruslan <chmilrv@users.noreply.github.com>
2020-09-07 13:35:10 +0300

    Добавьте файлы проекта.

---
+++ C_sharp_DZ_11_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*С помощью класса XmlTextWriter напишите приложение, сохраняющее в XML-файл информацию о заказах.
+Каждый заказ представляет собой несколько товаров. Информацию, характеризующую заказы и товары
+необходимо разработать самостоятельно.
+Считайте информацию из XML-документа, полученного в первом задании с помощью классов XmlDocument
+и XmlTextReader и выведите полученную информацию на экран.*/
+namespace C_sharp_DZ_11_1
+{
+
+
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
+}
