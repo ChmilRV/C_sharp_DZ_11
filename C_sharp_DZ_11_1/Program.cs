b02f0ef6b3cd522e89302eb54dc864b8e1e63a92
Chmil Ruslan <chmilrv@users.noreply.github.com>
2020-09-08 14:39:35 +0300

    1439

--- C_sharp_DZ_11_1/Program.cs
+++ C_sharp_DZ_11_1/Program.cs
@@ -7,12 +7,10 @@ using System.Threading.Tasks;
 using System.Xml;
 /*С помощью класса XmlTextWriter напишите приложение, сохраняющее в XML-файл информацию о заказах.
 Каждый заказ представляет собой несколько товаров. Информацию, характеризующую заказы и товары
-необходимо разработать самостоятельно.
-Считайте информацию из XML-документа, полученного в первом задании с помощью классов XmlDocument
-и XmlTextReader и выведите полученную информацию на экран.*/
+необходимо разработать самостоятельно.*/
 namespace C_sharp_DZ_11_1
 {
-    public class Goods        //Товары
+    public class Goods
     {
         public int Article { get; set; }
         public string Name { get; set; }
@@ -40,7 +38,6 @@ namespace C_sharp_DZ_11_1
             WriteLine($"Стоимость: {Cost}");
         }
     }
-
     public class Order
     {
         public List<Goods> List { get; set; }
@@ -51,11 +48,41 @@ namespace C_sharp_DZ_11_1
             List = list;
             OrderNumber = orderNumber;
         }
+        public void WriteOrder(string orderFileName)
+        {
+            XmlTextWriter order = null;
+            try
+            {
+                order = new XmlTextWriter(orderFileName, System.Text.Encoding.Unicode);
+                order.Formatting = Formatting.Indented;
+                order.WriteStartDocument();
+                order.WriteStartElement("Заказ");
+                order.WriteStartElement("Номер " + OrderNumber.ToString());
+                foreach (Goods goods in List)
+                {
+                    order.WriteStartElement("Товар");
+                    order.WriteAttributeString("Артикул", goods.Article.ToString());
+                    order.WriteElementString("Наименование", goods.Name);
+                    order.WriteElementString("Цена", goods.Price.ToString());
+                    order.WriteElementString("Количество", goods.Count.ToString());
+                    order.WriteElementString("Стоимость", goods.Cost.ToString());
+                    order.WriteEndElement();
+                }
+                order.WriteEndElement();
+                order.WriteEndElement();
+
+                WriteLine($"The {orderFileName} file is generated!");
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (order != null) order.Close();
+            }
+        }
     }
-
-
-
-
     class Program
     {
         static void Main(string[] args)
@@ -67,43 +94,8 @@ namespace C_sharp_DZ_11_1
             OrderList1.Add(new Goods(345, "Товар3", 45.98, 39));
             OrderList1.Add(new Goods(456, "Товар4", 78.12, 83));
             Order Order1 = new Order(OrderList1,1);
-            foreach (Goods goods in OrderList1) goods.GoodsReport();
-
-
-
-
-
-            XmlTextWriter order = null;
-            try
-            {
-                order = new XmlTextWriter("Order.xml", System.Text.Encoding.Unicode);
-                order.Formatting = Formatting.Indented;
-                order.WriteStartDocument();
-                order.WriteStartElement("Заказ");
-                foreach (Goods goods in OrderList1)
-                {
-                    order.WriteStartElement("Товар");
-                    order.WriteElementString("Артикул", goods.Article.ToString());
-                    order.WriteElementString("Наименование", goods.Name);
-                    order.WriteElementString("Цена", goods.Price.ToString());
-                    order.WriteElementString("Количество", goods.Count.ToString());
-                    order.WriteElementString("Стоимость", goods.Cost.ToString());
-                    order.WriteEndElement();
-                }
-                order.WriteEndElement();
-
-                WriteLine("The foods.xml file is generated!");
-            }
-            catch (Exception ex)
-            {
-                WriteLine(ex.Message);
-            }
-            finally
-            {
-                if (order != null) order.Close();
-            }
-
-
+            //foreach (Goods goods in OrderList1) goods.GoodsReport();
+            Order1.WriteOrder("Order1.xml");
             ReadKey();
         }
     }
---
+++ C_sharp_DZ_11_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+/*Считайте информацию из XML-документа, полученного в первом задании с помощью классов
+XmlDocument и XmlTextReader и выведите полученную информацию на экран.*/
+namespace C_sharp_DZ_11_2
+{
+    class Program
+    {
+        static void OutputNode(XmlNode node)
+        {
+            Console.WriteLine($"Type={node.NodeType}\tName={node.Name}\tValue={node.Value}");
+
+            // Внесем изменения в метод OutputNode(), чтобы 
+            // атрибуты выводились наравне с другими типами узлов.
+            if (node.Attributes != null)
+            {
+                foreach (XmlAttribute attr in node.Attributes)
+                    Console.WriteLine($"Type={attr.NodeType}\tName={attr.Name}\tValue={attr.Value}");
+            }
+            if (node.HasChildNodes)
+            {
+                foreach (XmlNode child in node.ChildNodes)
+                    OutputNode(child);
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+
+                doc.Load("../../../C_sharp_DZ_11_1/bin/Debug/Order1.xml");
+                OutputNode(doc.DocumentElement);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+
+        }
+    }
+}
